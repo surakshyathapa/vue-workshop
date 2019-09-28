904e32f94535ab81a56b0ec64048d9a179d2c56b
Surakshya Thapa <surakshya@dryicesolutions.net>
2019-09-28 13:11:40 +0545

    add comment

--- src/components/PhotoComment.vue
+++ src/components/PhotoComment.vue
@@ -7,16 +7,22 @@
         </div>
         <div class="comment__content">
           <ul class="comment-list">
-            <li>
-              <span></span>
+            <li v-for="(item, index) in comments" :key="index">
+              <span>{{item}}</span>
             </li>
           </ul>
-          <div class="no-comments">
+          <div v-if="!comments.length" class="no-comments">
             <p>No comments added yet</p>
           </div>
         </div>
         <div class="comment-box">
-          <textarea class="form-control" rows="3" placeholder="Write a comment"></textarea>
+          <textarea
+            v-model="comment"
+            @keyup.enter="addcomment"
+            class="form-control"
+            rows="3"
+            placeholder="Write a comment"
+          ></textarea>
         </div>
       </div>
     </div>
@@ -25,7 +31,19 @@
 
 <script>
 export default {
-  name: "PhotoComment"
+  name: "PhotoComment",
+  data() {
+    return {
+      comment: "PhotoComment",
+      comments: []
+    };
+  },
+  methods: {
+    addcomment() {
+      this.comments.push(this.comment);
+      this.comment = "";
+    }
+  }
 };
 </script>
 
